e9ec369b9dfa0a3e095631dd67112ae4dc9c5947
Mitch Siegel <mitch.siegel.a@gmail.com>
2021-11-09 23:39:48 -0500

    lets break the repo

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -6,96 +6,126 @@ FILE *infile;
 
 enum token
 {
-    t_id,
-    t_equals,
-    t_literal,
-    t_assignment,
-    t_notEquals,
-    t_lThan,
-    t_gThan,
-    t_lThanE,
-    t_gThanE,
-    t_plus,
-    t_minus,
-    t_times,
-    t_divide,
-    t_openParen,
-    t_closeParen,
-    t_openCurly,
-    t_closeCurly,
-    t_if
+    t_constant,
+    t_name,
+    t_auto,
+    t_if,
+    t_unEquals,
+    t_unMinus,
+    t_unNot,
+    t_binOr,
+    t_binAnd,
+    t_binEquals,
+    t_binNotEquals,
+    t_binLThan,
+    t_binLThanE,
+    t_binGThan,
+    t_binGThanE,
+    t_binLShift,
+    t_binRShift,
+    t_binMinus,
+    t_binPlus,
+    t_binMod,
+    t_binMultiply,
+    t_binDivide,
+    t_singleQuote,
+    t_doubleQuote,
+    t_lCurly,
+    t_rCurly,
+    t_lParen,
+    t_rParen,
+    t_lSquare,
+    t_rSquare
+
 };
 
 char *token_names[] = {
-    "ID",
-    "Literal",
-    "Assignment",
-    "Equals",
-    "NotEquals",
-    "LessThan",
-    "GreaterThan",
-    "LessThanEquals",
-    "GreaterThanEquals",
-    "Plus",
-    "minus",
-    "Times",
-    "Divide",
-    "OpenParen",
-    "CloseParen",
-    "OpenCurly",
-    "CloseCurly",
-    "If"};
+    "Constant",
+    "Name",
+    "Auto",
+    "if",
+    "Unary Equals",
+    "Unary Minus",
+    "Unary Not",
+    "Binary Or",
+    "Binary And",
+    "Binary Equals",
+    "Binary Not Equals",
+    "Binary Less Than",
+    "Binary Less Than or Equal",
+    "Binary Greater Than",
+    "Binary Greater Than or Equal",
+    "Binary Left Shift",
+    "Binary Right Shift",
+    "Binary Minus",
+    "Binary Plus",
+    "Binary Modulo",
+    "Binary Multiply",
+    "Binary Divide",
+    "Single Quote",
+    "Double Quote",
+    "Left Curly",
+    "Right Curly",
+    "Left Paren",
+    "Right Paren",
+    "Left Square Bracket",
+    "Right Square Bracket"};
 
 enum production
 {
     p_program,
-    p_stmt_list,
-    p_stmt,
-    p_condition,
-    p_expression,
-    p_term,
-    p_factor,
-    p_expression_tail,
-    p_term_tail,
-    p_factor_tail,
-    p_assignment,
-    p_condition_op,
-    p_addition_op,
-    p_multiplication_op,
-    p_id,
-    p_literal
+    p_definition,
+    p_name,
+    p_constant,
+    p_ival,
+    p_statement,
+    p_auto,
+    p_semicolon,
+    p_rvalue,
+    p_lvalue,
+    p_assign,
+    p_unary,
+    p_binary,
+    p_alpha,
+    p_digit,
+    p_alphadigit,
+    p_lparen,
+    p_rparen,
+    p_lcurly,
+    p_rcurly,
+    p_lsquare,
+    p_rsquare
 };
 
 char *production_names[] = {
     "Program",
-    "Statement List",
+    "Definition",
+    "Name",
+    "Constant",
+    "Ival",
     "Statement",
-    "Condition",
-    "Expression",
-    "Term",
-    "Factor",
-    "Expression Tail",
-    "Term Tail",
-    "Factor Tail",
-    "Assignment",
-    "Condition Operator",
-    "Addition Operator",
-    "Multiplication Operator",
-    "ID",
-    "Literal"};
+    "Auto",
+    ";",
+    "rvalue",
+    "lvalue",
+    "=",
+    "unary op",
+    "binary op",
+    "alpha",
+    "digit",
+    "alphadigit",
+    "(",
+    ")",
+    "{",
+    "}",
+    "[",
+    "]"};
 
 #define BUF_SIZE 16
 char buffer[BUF_SIZE];
 int buflen;
 char inChar;
-
-char lookahead()
-{
-    long offset = ftell(infile);
-    char returnChar = fgetc(infile);
-    fseek(infile, offset, SEEK_SET);
-    return returnChar;
-}
+enum token currentToken;
 
 void trimWhitespace()
 {
@@ -140,10 +170,18 @@ void trimWhitespace()
     }
 }
 
-enum token scan()
+char lookahead()
 {
-    printf("called scan\n");
-    enum token currentToken;
+    trimWhitespace();
+    long offset = ftell(infile);
+    char returnChar = fgetc(infile);
+    fseek(infile, offset, SEEK_SET);
+    return returnChar;
+}
+
+
+void scan()
+{
     buflen = 0;
     // check if we're looking for whitespace - are we?
     trimWhitespace();
@@ -163,25 +201,31 @@ enum token scan()
         buffer[buflen] = '\0';
         if (!strcmp(buffer, "if"))
         {
-            return t_if;
+            currentToken = t_if;
+            return;
+        }
+        else if (!strcmp(buffer, "auto"))
+        {
+            currentToken = t_auto;
+            return;
         }
 
         switch (inChar)
         {
         case '(':
-            currentToken = t_openParen;
+            currentToken = t_lParen;
             scanning = 0;
             break;
         case ')':
-            currentToken = t_closeParen;
+            currentToken = t_rParen;
             scanning = 0;
             break;
         case '{':
-            currentToken = t_openCurly;
+            currentToken = t_lCurly;
             scanning = 0;
             break;
         case '}':
-            currentToken = t_closeCurly;
+            currentToken = t_rCurly;
             scanning = 0;
             break;
         case '1':
@@ -194,62 +238,147 @@ enum token scan()
         case '8':
         case '9':
         case '0':
-            currentToken = t_literal;
-            break;
-        case '+':
-            currentToken = t_plus;
-            scanning = 0;
-            break;
-        case '-':
-            currentToken = t_minus;
-            scanning = 0;
-            break;
-        case '*':
-            currentToken = t_times;
-            scanning = 0;
-            break;
-        case '/':
-            currentToken = t_divide;
-            scanning = 0;
+            if (buflen == 1)
+            {
+                currentToken = t_constant;
+            }
+            else if (currentToken != t_name)
+            {
+                printf("Error - unexpected char [%c] in token [%s] \n", inChar, buffer);
+            }
             break;
         case '=':
-            if (lookahead() != '=')
+            switch (currentToken)
             {
-                if (buflen > 2)
+            case t_unNot:
+                currentToken = t_binNotEquals;
+                scanning = 0;
+                break;
+            case t_binLThan:
+                currentToken = t_binLThanE;
+                scanning = 0;
+                break;
+            case t_binGThan:
+                currentToken = t_binGThanE;
+                scanning = 0;
+                break;
+            default:
+                if (lookahead() != '=')
                 {
-                    printf("ERROR: More than 2 [=] in a row!\n");
-                    exit(1);
+                    if (buflen > 2)
+                    {
+                        printf("ERROR: More than 2 [=] in a row!\n");
+                        exit(1);
+                    }
+                    else if (buflen == 2)
+                    {
+                        currentToken = t_binEquals;
+                        scanning = 0;
+                    }
+                    else
+                    {
+                        currentToken = t_unEquals;
+                        scanning = 0;
+                    }
                 }
-                else if (buflen == 2)
+                break;
+            }
+            break;
+
+        case '|':
+            currentToken = t_binOr;
+            scanning = 0;
+            break;
+
+        case '&':
+            currentToken = t_binAnd;
+            scanning = 0;
+            break;
+
+        case '!':
+            currentToken = t_unNot;
+            if (lookahead() != '=') // only thing that can extend ! in a token is an =
+            {
+                scanning = 0;
+            }
+            break;
+
+        case '<':
+            if (buflen == 1) // only 1 '<' so far
+            {
+                currentToken = t_binLThan;
+                if (lookahead() != '=' && lookahead() != '<') // only thing that can extend < in a token is an = or <
                 {
-                    currentToken = t_equals;
-                    scanning = 0;
-                }
-                else
-                {
-                    currentToken = t_assignment;
                     scanning = 0;
                 }
             }
+            else // only way to have a < as the second char is to have lShift token
+            {
+                currentToken = t_binLShift;
+                scanning = 0;
+            }
             break;
-        case -1:
-        //case '\0':
-        //case ' ':
-        //case '\n':
-        //case '\t':
-            buflen--;
-            buffer[buflen] = '\0';
-        // don't include whitespace in the buffer
+
+        case '>':
+            if (buflen == 1)
+            {
+                currentToken = t_binGThan;
+                if (lookahead() != '=' && lookahead() != '>') // only thing that can extend > in a token is an = or >
+                {
+                    scanning = 0;
+                }
+            }
+            else // only way to have a > as the second char is to have rShift token
+            {
+                currentToken = t_binRShift;
+                scanning = 0;
+            }
+            break;
+
+        case '-':
+            currentToken = t_binMinus;
             scanning = 0;
             break;
+
+        case '+':
+            currentToken = t_binPlus;
+            scanning = 0;
+            break;
+
+        case '%':
+            currentToken = t_binMod;
+            scanning = 0;
+            break;
+
+        case '*':
+            currentToken = t_binMultiply;
+            scanning = 0;
+            break;
+
+        case '/':
+            currentToken = t_binDivide;
+            scanning = 0;
+            break;
+
+        case -1:
+            //case '\0':
+            //case ' ':
+            //case '\n':
+            //case '\t':
+            buflen--;
+            buffer[buflen] = '\0';
+            // don't include whitespace in the buffer
+            scanning = 0;
+            break;
+
         default:
-            currentToken = t_id;
+            currentToken = t_name;
             break;
         }
 
         // if there's a paren or bracket coming up
         // terminate the scanning, our token is done
-        printf("LOOKAHEAD IS %c : %d\n", lookahead());
+        //printf("LOOKAHEAD IS %c : %d\n", lookahead());
         switch (lookahead())
         {
         case '(':
@@ -264,7 +393,6 @@ enum token scan()
             break;
         }
     }
-    return currentToken;
 }
 
 struct PSNode
@@ -329,17 +457,25 @@ void stackParse()
     parseStackPush(stack, p_program);
     while (stack->size > 0)
     {
+        scan();
         switch (parseStackPop(stack))
         {
         case p_program:
-            parseStackPush(stack, p_stmt_list);
+            parseStackPush(stack, p_definition);
             // need to include EOF token or not?
             break;
-        case p_stmt_list:
-            switch (scan())
+        case p_definition:
+            switch (currentToken)
             {
-            case t_id:
-            case t_if:
+            case t_name:
+                switch (lookahead())
+                {
+
+                }
+                break;
+            default:
+                printf("Unexpected token %s [%s], expected [%s]\n", token_names[currentToken], buffer, token_names[t_name]);
+                exit(0);
             }
         default:
             break;
@@ -351,11 +487,12 @@ int main(int argc, char **argv)
 {
     printf("%s\n", argv[1]);
     infile = fopen(argv[1], "rb");
-    //struct parseStack *stack = malloc(sizeof(struct parseStack));
-    //parseStackPush(stack, p_program);
-    //stackParse();
+    struct parseStack *stack = malloc(sizeof(struct parseStack));
+    parseStackPush(stack, p_program);
+    stackParse();
     //char inc = fgetc(infile);
-    while (infile != EOF)
+
+    /*while (lookahead() != EOF)
     {
 
         //printf("%c\n", lookahead());
@@ -365,5 +502,5 @@ int main(int argc, char **argv)
         {
         }
         //inc = fgetc(infile);
-    }
+    }*/
 }
