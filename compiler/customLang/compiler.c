08ed1c9fa94b137472db56dbd706a8c06d06f825
Mitch Siegel <mitch.siegel.a@gmail.com>
2021-12-04 21:02:57 -0500

    expression parsing works now

--- compiler/customLang/compiler.c
+++ compiler/customLang/compiler.c
@@ -13,28 +13,29 @@ enum token
     t_literal,
     t_unOp,
     t_binOp,
-    t_unAssign,
     t_assign,
     t_semicolon,
     t_lParen,
     t_rParen,
     t_lCurly,
-    t_rCurly
+    t_rCurly,
+    t_EOF
 };
 
 char *token_names[] = {
     "var",
     "function",
     "name",
+    "literal",
     "unary operator",
     "binary operator",
-    "unary assignment",
     "assignment",
     "semicolon",
     "l paren",
     "r paren",
     "l curly",
-    "r curly"};
+    "r curly",
+    "EOF"};
 
 enum production
 {
@@ -67,51 +68,6 @@ char buffer[BUF_SIZE];
 int buflen;
 char inChar;
 
-/*
-void parseStackPush(struct parseStack *it, enum production theProduction)
-{
-    printf("Pushing production of type [%s]\n", production_names[theProduction]);
-    struct PSNode *newNode = malloc(sizeof(struct PSNode));
-    newNode->productionType = theProduction;
-    if (it->size == 0)
-    {
-        it->bottom = newNode;
-        it->top = newNode;
-    }
-    else
-    {
-        it->top->next = newNode;
-        newNode->prev = it->top;
-        it->top = newNode;
-    }
-    it->size++;
-}
-
-enum production parseStackPop(struct parseStack *it)
-{
-    if (it->size == 0)
-    {
-        printf("Error - tried to pop from empty stack\n");
-        exit(1);
-    }
-    struct PSNode *poppedNode;
-    if (it->size == 1)
-    {
-        poppedNode = it->top;
-        it->top = NULL;
-        it->bottom = NULL;
-    }
-    else
-    {
-        poppedNode = it->top;
-        it->top = it->top->prev;
-    }
-    it->size--;
-    enum production retType = poppedNode->productionType;
-    free(poppedNode);
-    return retType;
-}
-*/
 struct astNode
 {
     char *value;
@@ -144,7 +100,6 @@ void astNode_insertSibling(struct astNode *it, struct astNode *newSibling)
 
 void astNode_insertChild(struct astNode *it, struct astNode *newChild)
 {
-
     if (it->child == NULL)
     {
         it->child = newChild;
@@ -157,19 +112,15 @@ void astNode_insertChild(struct astNode *it, struct astNode *newChild)
 
 void printAST(struct astNode *it, int depth)
 {
-    if (it->child != NULL)
-    {
-        printAST(it->child, depth + 1);
-    }
-    for (int i = 0; i < depth; i++)
-    {
-        printf("\t");
-    }
-    printf("%s \n", it->value);
     if (it->sibling != NULL)
-    {
         printAST(it->sibling, depth);
-    }
+
+    for (int i = 0; i < depth; i++)
+        printf("\t");
+
+    printf("%s \n", it->value);
+    if (it->child != NULL)
+        printAST(it->child, depth + 1);
 }
 
 char lookahead_dumb()
@@ -243,7 +194,8 @@ char *reserved[] = {
     ";",
     "=",
     "+",
-    "-"};
+    "-",
+    "$$"};
 
 enum token reserved_t[] = {
     t_var,
@@ -253,39 +205,39 @@ enum token reserved_t[] = {
     t_lCurly,
     t_rCurly,
     t_semicolon,
-    t_unAssign,
+    t_assign,
     t_unOp,
-    t_unOp};
+    t_unOp,
+    t_EOF};
 
 enum token scan()
 {
     buflen = 0;
-    // check if we're looking for whitespace - are we?
+    // check if we're looking at whitespace - are we?
     trimWhitespace();
-    int scanning;
-    if (!feof(infile))
-    {
-        scanning = 1;
-    }
-    else
-    {
-        scanning = 0;
-    }
+    if (feof(infile))
+        return t_EOF;
+
     enum token currentToken;
-    while (scanning)
+
+    while (1)
     {
+        for (int i = 0; i < 0xffffff; i++)
+        {
+        }
         inChar = fgetc(infile);
+        if (feof(infile))
+        {
+            return currentToken;
+        }
         buffer[buflen++] = inChar;
         buffer[buflen] = '\0';
         // Iterate all reserved keywords
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 11; i++)
         {
             // if we match a reserved keyword
             if (!strcmp(buffer, reserved[i]))
-            {
-                // return its token
-                return reserved_t[i];
-            }
+                return reserved_t[i]; // return its token
         }
         if (buflen == 1)
         {
@@ -325,6 +277,14 @@ enum token scan()
     }
 }
 
+enum token lookaheadToken()
+{
+    long offset = ftell(infile);
+    enum token retToken = scan();
+    fseek(infile, offset, SEEK_SET);
+    return retToken;
+}
+
 struct astNode *match(enum token t)
 {
     enum token result = scan();
@@ -353,6 +313,7 @@ void consume(enum token t)
         printf("Error consuming - expected token [%s], got [%s] with image of [%s] instead!\n", token_names[t], token_names[result], buffer);
     }
 }
+
 struct astNode *parseProgram();
 struct astNode *parseTLDList();
 struct astNode *parseTLD();
@@ -370,113 +331,184 @@ struct astNode *parseProgram()
 struct astNode *parseTLDList()
 {
     struct astNode *TLDList = parseTLD();
-    while (!feof(infile))
+    while (1)
     {
+        if (lookaheadToken() == t_EOF)
+            break;
+        //printf("NEXT TOKEN IS\n %s\n", token_names[lookaheadToken()]);
         astNode_insertSibling(TLDList, parseTLD());
+        //trimWhitespace();
     }
+    printf("done parsing tld list\n");
     return TLDList;
 }
 
 struct astNode *parseTLD()
 {
     struct astNode *TLD;
-    switch (scan())
+    printf("IN TLD TOKEN IS %s\n", token_names[lookaheadToken()]);
+
+    switch (lookaheadToken())
     {
     case t_fun:
-        TLD = newastNode(t_fun, buffer);
+        TLD = match(t_fun);
         astNode_insertChild(TLD, match(t_name));
-        match(t_lParen);
-        match(t_rParen);
-        match(t_lCurly);
+        consume(t_lParen);
+        consume(t_rParen);
+        consume(t_lCurly);
         astNode_insertChild(TLD, parseStatementList());
-        match(t_rCurly);
+        consume(t_rCurly);
+        break;
+
     case t_var:
-        TLD = newastNode(t_var, buffer);
-        astNode_insertChild(TLD, match(t_name));
+    {
+        TLD = match(t_var);
+        struct astNode *name = match(t_name);
         if (lookahead() == '=')
         {
-            astNode_insertChild(TLD, match(t_unAssign));
-            astNode_insertChild(TLD, parseExpression());
+            struct astNode *assignment = match(t_assign);
+            astNode_insertChild(assignment, name);
+            astNode_insertChild(assignment, parseExpression());
+            astNode_insertChild(TLD, assignment);
+        }
+        else
+        {
+            astNode_insertChild(TLD, name);
         }
         consume(t_semicolon);
+    }
+    break;
+    default:
+        printf("Expected function or variable in Top Level Declaration");
+        exit(1);
     }
     return TLD;
 }
 
 struct astNode *parseVariableInit()
 {
+    return NULL;
 }
 
 struct astNode *parseAssignment()
 {
+    return NULL;
 }
 
 struct astNode *parseStatementList()
 {
+    struct astNode *stmtList = NULL;
+    int parsing = 1;
+    while (parsing)
+    {
+        struct astNode *stmt = NULL;
+        switch (lookaheadToken())
+        {
+        case t_var:
+        case t_name:
+            stmt = parseStatement();
+            if (stmtList == NULL)
+                stmtList = stmt;
+            else
+                astNode_insertSibling(stmtList, stmt);
+            break;
+        case t_rCurly:
+            parsing = 0;
+            break;
+        default:
+            exit(1);
+        }
+    }
+
+    printf("done parsing statement list");
+    return stmtList;
 }
 
 struct astNode *parseStatement()
 {
+    struct astNode *statement = NULL;
+    enum token upcomingToken = lookaheadToken();
+    switch (upcomingToken)
+    {
+    case t_var:
+    {
+        statement = match(t_var);
+        struct astNode *name = match(t_name);
+        if (lookaheadToken() == t_assign)
+        {
+            struct astNode *assign = match(t_assign);
+            astNode_insertChild(assign, name);
+            astNode_insertChild(assign, parseExpression());
+            astNode_insertChild(statement, assign);
+        }
+        else
+        {
+            astNode_insertChild(statement, name);
+        }
+        consume(t_semicolon);
+        break;
+    }
+    default:
+        printf("Error parsing statement - saw token [%s]\n", token_names[upcomingToken]);
+        exit(1);
+    }
+    printf("Done parsing statement- heres what we got\n");
+    printAST(statement, 0);
+    return statement;
 }
 
 struct astNode *parseExpression()
 {
     printf("parsing expression\n");
     struct astNode *expression = NULL;
+    struct astNode *lSide = NULL;
     if (isalpha(lookahead()))
-        {
-            expression = match(t_name);
-        }
-        else if (isdigit(lookahead()))
-        {
-            expression = match(t_literal);
-        }
-        else
-        {
-            printf("Error - expected literal or name in expression and got [%s]\n", token_names[scan()]);
-            exit(1);
-        }
-    while (1)
+        lSide = match(t_name);
+    else if (isdigit(lookahead()))
+        lSide = match(t_literal);
+    else if (lookahead() == '(')
     {
-        switch (lookahead())
-        {
-        case '+':
-        case '-':
-            astNode_insertSibling(expression, match(t_unOp));
-            break;
-        case ';':
-            printf("done parsing expression - here's what we got:\n");
-            printAST(expression, 0);
-            return expression;
-            break;
-        default:
-            printf("Error - expected unary operator in expression and got [%s]\n", token_names[scan()]);
-            exit(1);
-            break;
-        }
-
-        if (isalpha(lookahead()))
-        {
-            astNode_insertSibling(expression, match(t_name));
-        }
-        else if (isdigit(lookahead()))
-        {
-            astNode_insertSibling(expression, match(t_literal));
-        }
-        else
-        {
-            printf("Error - expected literal or name in expression and got [%s]\n", token_names[scan()]);
-            exit(1);
-        }
+        consume(t_lParen);
+        lSide = parseExpression();
+        consume(t_rParen);
     }
-}
+    else
+    {
+        printf("Error - expected literal or name in expression and got [%s]\n", token_names[scan()]);
+        exit(1);
+    }
 
+    switch (lookahead())
+    {
+    case '+':
+    case '-':
+        expression = match(t_unOp);
+        astNode_insertChild(expression, lSide);
+        astNode_insertChild(expression, parseExpression());
+        break;
+    case ';':
+    case ')':
+        printf("done parsing expression - here's what we got:\n");
+        expression = lSide;
+        printAST(expression, 0);
+        return expression;
+        break;
+    default:
+        printf("Error - expected unary operator in expression and got [%s]\n", token_names[scan()]);
+        exit(1);
+        break;
+    }
+    printf("done parsing expression - here's what we got:\n");
+    printAST(expression, 0);
+    return expression;
+}
 
 int main(int argc, char **argv)
 {
     printf("%s\n", argv[1]);
     infile = fopen(argv[1], "rb");
     struct astNode *program = parseProgram();
+    printf("DONE PARSING PROGRAM\n");
     printAST(program, 0);
 
     /*while (lookahead() != EOF)
